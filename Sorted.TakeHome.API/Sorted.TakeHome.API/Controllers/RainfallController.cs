283a4f9c0df36829abc2c8b47b199cbe1613f9f5
gianluca-zyla <gianluca.cerritelli@zyla.studio>
2024-07-19 18:13:07 +0100

    Create API project. Rainfall controller

---
+++ Sorted.TakeHome.API/Sorted.TakeHome.API/Controllers/RainfallController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sorted.TakeHome.API.Controllers
+{
+    public class RainfallController : Controller
+    {
+        public RainfallController() { }
+
+
+        [HttpGet("/rainfall/id/{stationId}/readings")]
+        public IEnumerable<int> GetStationReadings(string stationId, int count = 10)
+        {
+            return Array.Empty<int>();
+        }
+    
+        
+
+    }
+}
