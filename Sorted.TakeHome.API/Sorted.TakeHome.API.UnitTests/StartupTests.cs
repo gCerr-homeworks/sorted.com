08f7278fbca5587f4b9c05ba4262f4cb49cd8a3f
gCerr-homeworks <cerrphilatelist@gmail.com>
2024-07-22 09:35:42 +0100

    Add startup unit tests

---
+++ Sorted.TakeHome.API/Sorted.TakeHome.API.UnitTests/StartupTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sorted.TakeHome.API.Controllers;
+using Sorted.TakeHome.API.Readings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorted.TakeHome.API.UnitTests
+{
+      
+
+    public class StartupTests
+    {
+        private IConfiguration Configuration { get; }
+
+        public StartupTests()
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.development.json")
+                .Build();
+        }
+
+        [Fact]
+        public void canCreate_RainfallController()
+        {
+            var startup = new Startup(Configuration);
+
+            var services = new ServiceCollection();
+            services.AddScoped<IConfiguration>(a => Configuration);
+
+            startup.ConfigureServices(services);
+            var sp = services.BuildServiceProvider();
+
+            var controller = ActivatorUtilities.GetServiceOrCreateInstance<RainfallController>(sp);
+            controller.Should().NotBeNull();
+        }
+                                     }
+}
